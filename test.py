99510b0e209e935c8704f53ff6ffa3360901c207
Richard Board <rtboard1@gmail.com>
2025-09-29 13:13:53 +1300

    Test Vercel 2

--- test.py
+++
@@ -1,7 +0,0 @@
-catPlot = [
-    "Thinking about pushing a glass of the table.",
-    "Just left a hairball on your clean washing.",
-    "Tonight will summon the neighborhood cats for a midnight coup.",
-    "Knock over your coffee when you’re not looking."]
-
-print(catPlot[0])
